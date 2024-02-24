45bf8ed14e4165c71bec95f1d81ebb42a7873517
ejt7508 <ejt7508@rit.edu>
2024-02-24 14:21:00 -0500

    wrote get and post in view.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import persistence
 
 def goals(request):
     if request.method == "GET":
+        body = request.body
+        try:
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Request contains invalid JSON"}, status=400)
+        try:
+            json.loads(persistence.read_goals())
+            return JsonResponse({"message": "Read goal"}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Response contains invalid JSON"}, status=400)
         pass
     elif request.method == "POST":
-        pass
+        body = request.body
+        try:
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Request contains invalid JSON"}, status=400)
+        try:
+            json.loads(persistence.write_goal())
+            return JsonResponse({"message": "Wrote goal"}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Response contains invalid JSON"}, status=400)
     elif request.method == "PUT":
         pass
