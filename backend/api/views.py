ad1433ec6c96d9b7297caebd7ab519bb16c59134
Dubomb <dubomb1011@gmail.com>
2024-02-24 14:28:36 -0500

    Created test GET request to read from a data file

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import persistence
+from django.http import JsonResponse
+from . import persistence
 
 def goals(request):
     if request.method == "GET":
-        pass
+        goals = persistence.read_goals()
+        return JsonResponse({"message": goals}, status=400)
     elif request.method == "POST":
         pass
     elif request.method == "PUT":
