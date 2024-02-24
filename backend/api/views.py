e9bf94643ee50c75550a0f2722600a7392838db1
ejt7508 <ejt7508@rit.edu>
2024-02-24 14:29:44 -0500

    added put request method in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse, JsonResponse
 import persistence
 
 def goals(request):
+    body = request.body
+    try:
+        data = json.loads(body)
+    except json.JSONDecodeError:
+        return JsonResponse({"message": "Request contains invalid JSON"}, status=400)
+
     if request.method == "GET":
-        body = request.body
-        try:
-            data = json.loads(body)
-        except json.JSONDecodeError:
-            return JsonResponse({"message": "Request contains invalid JSON"}, status=400)
         try:
             json.loads(persistence.read_goals())
             return JsonResponse({"message": "Read goal"}, status=200)
@@ -16,15 +17,14 @@ def goals(request):
             return JsonResponse({"message": "Response contains invalid JSON"}, status=400)
         pass
     elif request.method == "POST":
-        body = request.body
-        try:
-            data = json.loads(body)
-        except json.JSONDecodeError:
-            return JsonResponse({"message": "Request contains invalid JSON"}, status=400)
         try:
             json.loads(persistence.write_goal())
             return JsonResponse({"message": "Wrote goal"}, status=200)
         except json.JSONDecodeError:
             return JsonResponse({"message": "Response contains invalid JSON"}, status=400)
     elif request.method == "PUT":
-        pass
+        try:
+            json.loads(persistence.update_goal())
+            return JsonResponse({"message": "Update goal"}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Response contains invalid JSON"}, status=400)
