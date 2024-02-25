f23a2cecb149c117c0cbb72d03affeb07b8ca1ae
Dubomb <dubomb1011@gmail.com>
2024-02-25 07:22:54 -0500

    Added goal creation menu to tiger pledge page

--- frontend/src/components/GoalCreateMenu.vue
+++ frontend/src/components/GoalCreateMenu.vue
@@ -1,9 +1,38 @@
 <template>
-    <h2>Bottom</h2>
+    <h2>Create a goal!</h2>
+    <form @submit.prevent="onSubmit" @submit="$emit('createGoal', buildGoal())">
+        <label>Title:</label>
+        <input type="text" required="true" v-model="title"/>
+        <label>Description:</label>
+        <input type="text" required="true" v-model="description"/>
+        <label>Goal amount:</label>
+        <input type="number" required="true" min="1" v-model="goal_amount"/>
+        <button type="submit">Create</button>
+    </form>
 </template>
 
 <script>
 export default {
     name: "GoalCreateMenu",
+
+    data() {
+        return {
+            title: "",
+            description: "",
+            goal_amount: "",
+        }
+    },
+
+    methods: {
+        onSubmit() {
+            this.title = "";
+            this.description = "";
+            this.goal_amount = "";
+        },
+
+        buildGoal() {
+            return {"title": this.title, "description": this.description, "current_amount": 0, "goal_amount": this.goal_amount};
+        },
+    }
 }
 </script>
