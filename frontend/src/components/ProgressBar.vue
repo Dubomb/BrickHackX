a52033fd88e39ba86cd2eb099403d2f2b8728cbc
sz7364 <sz7364@g.rit.edu>
2024-02-24 21:37:14 -0500

    progress bar functional, but not complete

---
+++ frontend/src/components/ProgressBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="progress-container">
+        <h2>Progress</h2>
+        <div class="progress-bar" :style="{ width: progress + '%' }"></div>
+    <div class="controls">
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            progress: 0 // all goal progress starts at 0
+        };
+    },
+
+    methods: {
+        increment() {
+            if (this.progress < 100) {  // need to replace with actual goal instead of 100
+                this.progress += 10; 
+            }
+        },
+
+        decrement() {
+            if (this.progress > 0) {
+                this.progress -= 10;
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.progress-container {
+  width: 100%;
+}
+
+.progress-bar {
+  height: 20px;
+  background-color: #42b983; 
+}
+
+.controls {
+  margin-top: 10px;
+}
+
+button {
+  margin-right: 10px;
+}
+</style>
